286fe4a275b56d51e3e5a3de8d8be6f5bc7bc51e
cdntinker <cdntinker@gmail.com>
2021-07-12 19:01:41 -0400

    0

--- src/SiniLink_functions.cpp
+++ src/SiniLink_functions.cpp
@@ -38,51 +38,49 @@ bool SiniLink_PWR_STATE;
 bool SiniLink_LED01_STATE;
 bool SiniLink_LED02_STATE;
 
-#ifndef TestCode
-void MQTT_HandleMessages(const char *Topic, const char Message[MQTT_BUFFER_SIZE])
-{
-    if (strcmp(Topic, "/Power") == 0)
-    {
-        // MQTT_SendTELE(Topic, Topic);
-        MQTT_SendNOTI("triggered", "Power!!!");
-        if (strcmp(Message, "on") == 0)
-            SiniLink_Relay(HIGH);
-        if (strcmp(Message, "off") == 0)
-            SiniLink_Relay(LOW);
-        if (strcmp(Message, "toggle") == 0)
-            SiniLink_Toggle();
-        // SiniLink_Relay(!SiniLink_PWR_STATE);
-    }
-    else if (strcmp(Topic, "/LED01") == 0)
-    {
-        // MQTT_SendTELE(Topic, Topic);
-        MQTT_SendNOTI("triggered", "LED01!!!");
-        if (strcmp(Message, "on") == 0)
-            SiniLink_LED(HIGH);
-        if (strcmp(Message, "off") == 0)
-            SiniLink_LED(LOW);
-        if (strcmp(Message, "toggle") == 0)
-            SiniLink_LED(!SiniLink_LED01_STATE);
-    }
-    else if (strcmp(Topic, "/LED02") == 0)
-    {
-        // MQTT_SendTELE(Topic, Topic);
-        MQTT_SendNOTI("triggered", "LED02!!!");
-        if (strcmp(Message, "on") == 0)
-            SiniLink_LINKLED(HIGH);
-        if (strcmp(Message, "off") == 0)
-            SiniLink_LINKLED(LOW);
-        if (strcmp(Message, "toggle") == 0)
-            SiniLink_LINKLED(!SiniLink_LED02_STATE);
-    }
+// void MQTT_HandleMessages(const char *Topic, const char Message[MQTT_BUFFER_SIZE])
+// {
+//     if (strcmp(Topic, "/Power") == 0)
+//     {
+//         // MQTT_SendTELE(Topic, Topic);
+//         MQTT_SendNOTI("triggered", "Power!!!");
+//         if (strcmp(Message, "on") == 0)
+//             SiniLink_Relay(HIGH);
+//         if (strcmp(Message, "off") == 0)
+//             SiniLink_Relay(LOW);
+//         if (strcmp(Message, "toggle") == 0)
+//             SiniLink_Toggle();
+//         // SiniLink_Relay(!SiniLink_PWR_STATE);
+//     }
+//     else if (strcmp(Topic, "/LED01") == 0)
+//     {
+//         // MQTT_SendTELE(Topic, Topic);
+//         MQTT_SendNOTI("triggered", "LED01!!!");
+//         if (strcmp(Message, "on") == 0)
+//             SiniLink_LED(HIGH);
+//         if (strcmp(Message, "off") == 0)
+//             SiniLink_LED(LOW);
+//         if (strcmp(Message, "toggle") == 0)
+//             SiniLink_LED(!SiniLink_LED01_STATE);
+//     }
+//     else if (strcmp(Topic, "/LED02") == 0)
+//     {
+//         // MQTT_SendTELE(Topic, Topic);
+//         MQTT_SendNOTI("triggered", "LED02!!!");
+//         if (strcmp(Message, "on") == 0)
+//             SiniLink_LINKLED(HIGH);
+//         if (strcmp(Message, "off") == 0)
+//             SiniLink_LINKLED(LOW);
+//         if (strcmp(Message, "toggle") == 0)
+//             SiniLink_LINKLED(!SiniLink_LED02_STATE);
+//     }
 
-    else
-    {
-        DEBUG_Trouble("Dunno Whatcha want...");
-        MQTT_SendNOTI("Error", "Dunno Whatcha want...");
-    }
-}
-#endif
+//     else
+//     {
+//         DEBUG_Trouble("Dunno Whatcha want...");
+//         MQTT_SendNOTI("Error", "Dunno Whatcha want...");
+//     }
+// }
 
 // Detect button press
 void SiniLink_Button()
