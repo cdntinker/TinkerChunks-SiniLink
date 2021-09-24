eb1a6c60cc83d8f758681fe56cd9e1c1dba9c995
cdntinker <58090022+cdntinker@users.noreply.github.com>
2021-09-24 15:09:34 -0400

    Moved the #defines

--- include/Tinker_SiniLink.h
+++ include/Tinker_SiniLink.h
@@ -1,7 +1,13 @@
-// GPIO  4 = Button 1
-// GPIO  5 = Relay 1
-// GPIO 14 = LED 1
-// GPIO 16 = LEDLINK
+
+#ifndef SiniLink_DEFINES
+#define SiniLink_DEFINES
+
+#define SiniLink_BUTTN 4  // Button 1 - The button
+#define SiniLink_POWER 5  // Relay 1  - Switch the connections + red LED
+#define SiniLink_LED01 14 // LED 1    - Green LED
+#define SiniLink_LNKLD 16 // LEDLINK  - Blue LED
+
+#endif // SiniLink_DEFINES
 
 // bool TestState = LOW;
 
