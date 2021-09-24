71265740ef5c537404ea93de6f0baa3eda5c59cb
cdntinker <58090022+cdntinker@users.noreply.github.com>
2021-09-24 17:30:46 -0400

    Starting to generalise
    Added Sonoff Basic defines.

--- include/Tinker_SiniLink.h
+++ include/Tinker_SiniLink.h
@@ -1,13 +1,18 @@
 
 #ifndef SiniLink_DEFINES
 #define SiniLink_DEFINES
-
-#define SiniLink_BUTTN 4  // Button 1 - The button
-#define SiniLink_POWER 5  // Relay 1  - Switch the connections + red LED
-#define SiniLink_LED01 14 // LED 1    - Green LED
-#define SiniLink_LNKLD 16 // LEDLINK  - Blue LED
-
+  #define SiniLink_BUTTN 4  // Button 1 - The button
+  #define SiniLink_POWER 5  // Relay 1  - The Relay + red LED
+  #define SiniLink_LED01 14 // LED 1    - Green LED
+  #define SiniLink_LNKLD 16 // LEDLINK  - Blue LED
 #endif // SiniLink_DEFINES
+
+#ifndef SonoffBasic_DEFINES
+#define SonoffBasic_DEFINES
+  #define SonoffBasic_BUTTN 0  // Button 1 - The button
+  #define SonoffBasic_POWER 12 // Relay 1  - The Relay
+  #define SonoffBasic_LED01 13 // LED 1    - Green LED (inverted)
+#endif // SonoffBasic_DEFINES
 
 // bool TestState = LOW;
 
