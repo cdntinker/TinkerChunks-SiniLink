0f96d27168717de8e421009158a03c1f3459c4c1
cdntinker <58090022+cdntinker@users.noreply.github.com>
2021-09-24 18:25:21 -0400

    Update Tinker_SiniLink.h

--- include/Tinker_SiniLink.h
+++ include/Tinker_SiniLink.h
@@ -1,30 +1,30 @@
 
-#ifndef SiniLink_DEFINES
-#define SiniLink_DEFINES
-  #define SiniLink_BUTTN 4  // Button 1 - The button
-  #define SiniLink_POWER 5  // Relay 1  - The Relay + red LED
-  #define SiniLink_LED01 14 // LED 1    - Green LED
-  #define SiniLink_LNKLD 16 // LEDLINK  - Blue LED
-#endif // SiniLink_DEFINES
+#ifndef SmartSwitch_DEFINES
+#define SmartSwitch_DEFINES
+  #define SmartSwitch_BUTTN 4  // Button 1 - The button
+  #define SmartSwitch_POWER 5  // Relay 1  - The Relay + red LED
+  #define SmartSwitch_LED01 14 // LED 1    - Green LED
+  #define SmartSwitch_LNKLD 16 // LEDLINK  - Blue LED
+#endif // SmartSwitch_DEFINES
 
-#ifndef SonoffBasic_DEFINES
-#define SonoffBasic_DEFINES
-  #define SonoffBasic_BUTTN 0  // Button 1 - The button
-  #define SonoffBasic_POWER 12 // Relay 1  - The Relay
-  #define SonoffBasic_LED01 13 // LED 1    - Green LED (inverted)
-#endif // SonoffBasic_DEFINES
+// #ifndef SonoffBasic_DEFINES
+// #define SonoffBasic_DEFINES
+//   #define SonoffBasic_BUTTN 0  // Button 1 - The button
+//   #define SonoffBasic_POWER 12 // Relay 1  - The Relay
+//   #define SonoffBasic_LED01 13 // LED 1    - Green LED (inverted)
+// #endif // SonoffBasic_DEFINES
 
 // bool TestState = LOW;
 
-void SiniLink_test();
+void SmartSwitch_test();
 
-void SiniLink_init();
+void SmartSwitch_init();
 
-void SiniLink_Button();
-void SiniLink_Relay(bool OnOff);
-void SiniLink_Toggle();
+void SmartSwitch_Button();
+void SmartSwitch_Relay(bool OnOff);
+void SmartSwitch_Toggle();
 
-void SiniLink_LED(bool OnOff);
-void SiniLink_LINKLED(bool OnOff);
+void SmartSwitch_LED(bool OnOff);
+void SmartSwitch_LINKLED(bool OnOff);
 
 void MQTT_HandleMessages(const char *Topic, const char *Message);
